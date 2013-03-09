7c713ccb4ce1dc8d722e9294c84a5799b4166129
liguangyi <liguangyi@liguangyi.(none)>
2013-03-09 14:38:01 +0800

    server complete

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express')
+    fs = require('fs');
 var app = express();
 app.listen(8000);
 
@@ -7,8 +8,8 @@ app.configure(function(){
     app.use(express.bodyParser({
         uploadDir: "public/images",
         keepExtensions: true,
-        limit: 10000000, /* 10M  10*1000*1000 */  
-        defer: true 
+        limit: 10000000 /* 10M  10*1000*1000 */  
+        // defer: true 
     }));
     app.use(express.static(__dirname + "/public"));
 });
@@ -16,15 +17,22 @@ app.configure(function(){
 
 
 app.post("/upload", function (req, res) {
-    console.log(req.files);
     var image = req.files.image;
 
-    //首先要处理单个上传和多个上传
+    // fs.rename(image.path, "public/images/text.jpg", function () {
+    //     res.send(image);    
+    // });
+    
+    // req.form.on('progress', function(bytesReceived, bytesExpected) {
+    //     console.log(((bytesReceived / bytesExpected)*100) + "% uploaded");
+    // });
+
+    // req.form.on('end', function() {
+    //     console.log(req.files);
+    //     res.send("well done");
+    // });
+
+    // 首先要处理单个上传和多个上传
     // console.log(image);
     // console.log(image.length);
-
-    if (Array.isArray(image)) console.log("is array");
-    else console.log("is not array");
-
-    res.send("ok");
 })
