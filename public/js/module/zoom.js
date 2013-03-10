e7bc39211d76a376a97ac5294facb81dd1b2ca3b
invegas <juststayinvegas@gmail.com>
2013-03-11 00:34:03 +0800

    complete

--- public/js/module/zoom.js
+++ public/js/module/zoom.js
@@ -1,9 +1,36 @@
 window.zoom = (function () {
-	var _width, _count = 0, _percent = 10;	
+	var _width, _count = 0, _percent = 10, num = 1, temp = 0, w;
+	var isPress = 0, allowPress = 1;
+	var btn_pos_x, btn_pos_y, mou_pos_x, mouse_pos_y, diff;
 
 	var reset = function (e) {
 		_count = 0;
 		adjustZoom();
+	}
+
+	var widthPlus = function (val) {
+		_count = _count + val;
+	}
+
+	var widthMinus = function (val) {
+		_count = _count - val;
+	}
+
+	var getPercent = function () {
+		var w = _width + _count * _percent,
+			percent = parseFloat(w / _width);
+
+		return percent;
+	}
+
+	var showPercent = function (selector) {
+		var percent = parseFloat(w / _width);
+			percent = percent.toFixed(2);
+			percent = percent * 100;
+
+			percent = parseInt(percent);
+
+		selector.val(percent + "%");
 	}
 
 	var getWheelVal = function (e) {
@@ -12,17 +39,18 @@ window.zoom = (function () {
 		else _count++;		
 	}
 
-	var adjustZoom = function (e) {
-
-		var w = _width + _count * _percent;
+	var adjustZoom = function (val) {
+		// var w;
+		if (!val) w = _width + _count * _percent;
+		else w = _width + (_count + val) * _percent;
 		var percent = parseFloat(w / _width);
-		console.log(percent.toFixed(2));
 		$('img').css({
 	        'position': 'absolute',
 	        'left': '50%',
 	        'width': w + "px",
 	        'margin-left': w * (-1) / 2 + "px"
 	    });
+	    showPercent($('.input-width-val'));
 	}
 
 	var insertImg = function (src, selector) {
@@ -54,9 +82,44 @@ window.zoom = (function () {
 		})
 	}
 
+	var compute = function () {
+	    diff = mou_pos_x - btn_pos_x;
+	    zoom.adjustZoom(diff);
+	}
+
+	var initMouse = function (clas) {
+		$(document).on('mousedown', function (e) {
+			diff = 0
+		    var ev = e.originalEvent, target = $(e.target);
+		    if (target.hasClass(clas) || target.parent().hasClass(clas)) {
+		        isPress = 1;
+		        btn_pos_x = ev.clientX;
+		        btn_pos_y = ev.clientY;
+		    }
+		}).on('mouseup', function (e) {
+			zoom.widthPlus(diff);
+		    isPress = 0;
+		    allowPress = 1;
+		}).on('mousemove', function (e) {
+
+		    allowPress = 0;
+		    if (!isPress) return false;
+		    mou_pos_x = e.originalEvent.clientX;
+		    mou_pos_y = e.originalEvent.clientY;
+		    compute();
+
+		})
+	}
+
 	return {
+		adjustZoom: adjustZoom,
 		insertImg: insertImg,
 		initWheel: initWheel,
-		reset: reset
+		reset: reset,
+		widthPlus: widthPlus,
+		widthMinus: widthMinus,
+		getPercent: getPercent,
+		showPercent: showPercent,
+		initMouse: initMouse
 	}
 })()
